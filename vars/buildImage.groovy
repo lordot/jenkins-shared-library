e12033ac0f71b6b7901b1d2b388bdaa18d7ae93e
lordot <w.dmkvo@gmail.com>
2023-09-17 17:12:01 +0300

    init commit 2

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,5 +1,5 @@
 #! /usr/bin/env groovy
 
-def call(String imageName, String repo, String path = '.') {
-    sh "docker build $path -t $repo/$imageName"
+def call(String imageName) {
+    sh "docker build ./airbot -t $imageName"
 }
