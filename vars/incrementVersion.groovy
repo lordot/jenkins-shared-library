d1fd8a3efac972579d20291a746c7eaed7cf498f
lordot <w.dmkvo@gmail.com>
2023-09-18 01:00:51 +0300

    init commit 8

--- vars/incrementVersion.groovy
+++ vars/incrementVersion.groovy
@@ -20,7 +20,7 @@ def call(String Path = '.') {
         def newPatchVersion = versionParts[2].toInteger() + 1
         env.CURRENT_VERSION = "${versionParts[0]}.${versionParts[1]}.${newPatchVersion}"
     }
-    sh "sed -i 's/LABEL version=.*/LABEL version=\"${env.CURRENT_VERSION}\"/' ${Path}/Dockerfile"
+    sh "sed -i 's/LABEL version=.*/LABEL version=${env.CURRENT_VERSION}/' ${Path}/Dockerfile"
     echo "New version: ${env.CURRENT_VERSION}"
 }
 
