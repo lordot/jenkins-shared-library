92892492d99b5843af48e148f3a16993e9482589
lordot <w.dmkvo@gmail.com>
2023-09-17 17:50:11 +0300

    init commit 5

--- vars/pushImage.groovy
+++ vars/pushImage.groovy
@@ -1,7 +1,6 @@
 #! /usr/bin/env groovy
 
-def call(String Repo, String imageName) {
-    sh "echo $CREDS_PSW | docker login -u $CREDS_USR --password-stdin $Repo"
-    sh "docker push $Repo/$imageName"
-    sh "docker push $Repo/$imageName"
+def call(String imageName) {
+    sh "docker push $imageName"
+    sh "docker push $imageName"
 }
